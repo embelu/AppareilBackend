223216b93ce4f4adc0960a4c93fa11a65be72177
Ludovic Baens <ludovic.baens@solidaris.be>
2021-05-06 12:41:36 +0200

    Add Interfaces

---
+++ Appareil.BL/Models/AppareilDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Appareil.BL.Models
+{
+    class AppareilDTO
+    {
+    }
+}
---
+++ Appareil.Repository/Repositories/AppareilRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Appareil.Repository.Repositories
+{
+    class AppareilRepository
+    {
+    }
+}
